2f88ba0637609b2463cc5d541252e978e2e7a3dc
Robert Cerven <rcerven@redhat.com>
2017-01-04 17:58:37 +0100

    fix

--- tests/plugins/test_add_labels.py
+++ tests/plugins/test_add_labels.py
@@ -296,16 +296,30 @@ def test_add_labels_aliases(tmpdir, docker_tasker, caplog,
     if expected_log:
         assert expected_log in caplog.text()
 
-@pytest.mark.parametrize('labels', [{"distribution-scope": "restricted",
-                                     "dont_overwrite_if_in_dockerfile": "distribution-scope",
-                                     "auto_labels": [], 'aliases': {}},
-                                    {"distribution-scope": "restricted",
-                                     "auto_labels": [], 'aliases': {}}
+@pytest.mark.parametrize('parent_scope, docker_scope, result_scope, overwrite', [
+    (None, None, "restricted", False),
+    ("public", None, "restricted", False),
+    ("private", None, "restricted", False),
+    ("restricted", 'public', "public", False),
+    ("restricted", 'restricted', "restricted", False),
+    ("restricted", 'private', "private", False),
+    (None, None, "restricted", True),
+    ("public", None, "restricted", True),
+    ("private", None, "restricted", True),
+    ("restricted", 'public', "public", True),
+    ("restricted", 'restricted', "restricted", True),
+    ("restricted", 'private', "private", True)
 ])
-def test_dont_overwrite_distribution_scope(tmpdir, docker_tasker, labels):
+def test_dont_overwrite_distribution_scope(tmpdir, docker_tasker, parent_scope,
+                                           docker_scope, result_scope, overwrite):
     df_content = "FROM fedora\n"
-    df_content += 'LABEL distribution-scope="private"'
-    labels_conf_base = {INSPECT_CONFIG: {"Labels": {"distribution-scope": "private"}}}
+    if docker_scope:
+        df_content += 'LABEL distribution-scope="{0}"'.format(docker_scope)
+
+    if parent_scope:
+        labels_conf_base = {INSPECT_CONFIG: {"Labels": {"distribution-scope": parent_scope}}}
+    else:
+        labels_conf_base = {INSPECT_CONFIG: {"Labels": {}}}
 
     df = df_parser(str(tmpdir))
     df.content = df_content
@@ -318,16 +332,37 @@ def test_dont_overwrite_distribution_scope(tmpdir, docker_tasker, labels):
     flexmock(workflow, base_image_inspect=labels_conf_base)
     setattr(workflow.builder, 'df_path', df.dockerfile_path)
 
-    runner = PreBuildPluginsRunner(
-        docker_tasker,
-        workflow,
-        [{
-            'name': AddLabelsPlugin.key,
-            'args': {'labels': labels}
-        }]
-    )
+    if overwrite:
+        runner = PreBuildPluginsRunner(
+            docker_tasker,
+            workflow,
+            [{
+                'name': AddLabelsPlugin.key,
+                'args': {
+                    'labels': {"distribution-scope": "restricted"},
+                    'auto_labels': [],
+                    'dont_overwrite_if_in_dockerfile': "distribution-scope",
+                    'aliases': {},
+                }
+
+            }]
+        )
+    else:
+        runner = PreBuildPluginsRunner(
+            docker_tasker,
+            workflow,
+            [{
+                'name': AddLabelsPlugin.key,
+                'args': {
+                    'labels': {"distribution-scope": "restricted"},
+                    'auto_labels': [],
+                    'aliases': {},
+                }
+
+            }]
+        )
 
     runner.run()
 
     result = df.labels.get("distribution-scope")
-    assert result == "private" 
+    assert result == result_scope
